2e4ebe52125baf2e9f80747450c1d66d6c30f06f
ShivamSwaroop <dshivamswaroop@gmail.com>
2025-12-18 21:39:48 +0530

    (backend): registration API and mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
+connectDB();
 
 const app = express();
 
@@ -12,6 +15,8 @@ app.use(express.json());
 app.get("/", (rep, res)=>{
     res.send("Backend running successfully");
 });
+
+app.use("/api/auth", authRoutes);
 
 const PORT = 5000;
 app.listen(PORT, ()=>{
